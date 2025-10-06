24b3b5b9de94ac68215bb1bc6fbb64b5bc16a3db
jahvoid <akjasj0012@gmail.com>
2025-10-05 20:20:14 -0400

    Final Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function scrollToProjects(){
+    window.location.href = "projects.html";
+}
+
+function submitForm(event){
+    event.preventDefault();
+    alert("Thank you for you message! This is not a real form.");
+}
